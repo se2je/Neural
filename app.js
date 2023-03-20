dc93e79169e62b7e22cd730c1bdd36a94718da74
se2je <78136073+se2je@users.noreply.github.com>
2023-03-20 13:38:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const onTopBtn = document.querySelector('.onTopBtn')
+
+onTopBtn.addEventListener('click', function(){
+    window.scrollTo(0, 0)
+})
+
+
+const consoleText = "I was made by ChatGPT, say thanks!";
+const consoleEl = document.getElementById('console-text');
+let consoleTimerId;
+let index = 0;
+
+function printConsoleText() {
+  consoleEl.textContent += consoleText[index];
+  index++;
+
+  if (index >= consoleText.length) {
+    clearInterval(consoleTimerId);
+    setTimeout(function() {
+      eraseConsoleText();
+    }, 1000);
+  }
+}
+
+function eraseConsoleText() {
+  consoleTimerId = setInterval(function() {
+    consoleEl.textContent = consoleEl.textContent.slice(0, -1);
+
+    if (consoleEl.textContent === '') {
+      clearInterval(consoleTimerId);
+      index = 0;
+      setTimeout(function() {
+        printConsoleText();
+      }, 1000);
+    }
+  }, 50);
+}
+
+setTimeout(function() {
+  printConsoleText();
+}, 1000);
