3da6bab6525c1ddf1efe6b6ea5f96934de3b51d3
se2je <78136073+se2je@users.noreply.github.com>
2023-03-22 20:13:04 +0200

    0.2

--- app.js
+++ app.js
@@ -4,50 +4,15 @@ onTopBtn.addEventListener('click', function(){
     window.scrollTo(0, 0)
 })
 
-
-const consoleText = "I was made by ChatGPT, say thanks!";
-const consoleEl = document.getElementById('console-text');
-let consoleTimerId;
-let index = 0;
-
-function printConsoleText() {
-  consoleEl.textContent += consoleText[index];
-  index++;
-
-  if (index >= consoleText.length) {
-    clearInterval(consoleTimerId);
-    setTimeout(function() {
-      eraseConsoleText();
-    }, 1000);
-  }
-}
-
-function eraseConsoleText() {
-  consoleTimerId = setInterval(function() {
-    consoleEl.textContent = consoleEl.textContent.slice(0, -1);
-
-    if (consoleEl.textContent === '') {
-      clearInterval(consoleTimerId);
-      index = 0;
-      setTimeout(function() {
-        printConsoleText();
-      }, 1000);
-    }
-  }, 50);
-}
-
-setTimeout(function() {
-  printConsoleText();
-}, 1000);
-
-
-
-const burgerOpen = document.querySelectorAll('.burger')
+const burger = document.querySelectorAll('.burger')
 const header = document.querySelector('.header')
 
 burger.forEach(element => {
   element.addEventListener('click', function(){
     header.classList.toggle('show-header')
-    alert('')
   })
 });
+
+window.onload = function() {
+  document.body.classList.add('loaded');
+};
